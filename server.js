1ee6d4fa99b248c12956319fdfe0103163d2e7ef
Louis Hellman <louis.hellman@gmail.com>
2020-08-10 16:55:05 -0700

    updating db connection info

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const cors = require('cors');
 const db = require('knex')({
   client: 'pg',
   connection: {
-    host : 'postgresql-aerodynamic-00412',
-    user : 'louishellman',
-    password : '',
-    database : 'face-recognition'
+    host : 'process.env.DATABASE_URL',
+    ssl: true
   }
 });
 const register = require('./controllers/register');
